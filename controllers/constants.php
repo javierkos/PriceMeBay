94c2a35a3814ae15d878f57e78dee81a78ecd9df
javierkos <zcabpas@ucl.ac.uk>
2018-01-14 19:36:35 +0000

    Debugging DB connection

--- controllers/constants.php
+++ controllers/constants.php
@@ -1,9 +1,9 @@
 <?php
 
-define("HOST", "127.0.0.1"); 
+define("HOST", "127.0.0.1:53238"); 
 define("USER", "azure"); 
 define("PASSWORD", "6#vWHD_$");
-define("DATABASE", "pricemebay");
+define("DATABASE", "localdb");
  
 define("CAN_REGISTER", "any");
 define("DEFAULT_ROLE", "member");
