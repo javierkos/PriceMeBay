cc79bf6997b3be10e2810e350393d8c54fe92d2d
javierkos <zcabpas@ucl.ac.uk>
2018-01-15 16:08:49 +0000

    Finished URL escaping

--- js/list.js
+++ js/list.js
@@ -3,7 +3,7 @@ numPerPage = 9;
 currentSelPage = 1;
 $(document).ready(function() {
     url = window.location.href;
-    search = decodeURI(url.replace(/\+/g, "%20")).split("?s=")[1].replace(/[^0-9a-z]/gi, '');
+    search = unescape(url).split("?s=")[1].replace(/[^0-9a-z]/gi, '');
 
     if (search){
         $.ajax({
