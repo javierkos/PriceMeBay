ed01d92435ce95bb4f49e3ab6d244afdc8784e35
javierkos <zcabpas@ucl.ac.uk>
2018-01-15 06:04:14 +0000

    Test ebay sdk/API
    Copied some code to test the API with the credentials generated, and
    some code from
    https://github.com/davidtsadler/ebay-sdk-examples/blob/master/configurat
    ion.php.example to test an sdk.

---
+++ controllers/searchItems.php
@@ -0,0 +1,57 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
+
+require __DIR__.'/../vendor/autoload.php';
+
+$config = require __DIR__.'/../configuration.php';
+/**
+ * The namespaces provided by the SDK.
+ */
+use \DTS\eBaySDK\Constants;
+use \DTS\eBaySDK\Finding\Services;
+use \DTS\eBaySDK\Finding\Types;
+use \DTS\eBaySDK\Finding\Enums;
+/**
+ * Create the service object.
+ */
+$service = new Services\FindingService([
+    'credentials' => $config['sandbox']['credentials'],
+    'globalId'    => Constants\GlobalIds::GB
+]);
+/**
+ * Create the request object.
+ */
+$request = new Types\FindItemsByKeywordsRequest();
+/**
+ * Assign the keywords.
+ */
+$request->keywords = 'Harry Potter';
+/**
+ * Send the request.
+ */
+$response = $service->findItemsByKeywords($request);
+/**
+ * Output the result of the search.
+ */
+if (isset($response->errorMessage)) {
+    foreach ($response->errorMessage->error as $error) {
+        printf(
+            "%s: %s\n\n",
+            $error->severity=== Enums\ErrorSeverity::C_ERROR ? 'Error' : 'Warning',
+            $error->message
+        );
+    }
+}
+if ($response->ack !== 'Failure') {
+    foreach ($response->searchResult->item as $item) {
+        printf(
+            "(%s) %s: %s %.2f\n",
+            $item->itemId,
+            $item->title,
+            $item->sellingStatus->currentPrice->currencyId,
+            $item->sellingStatus->currentPrice->value
+        );
+        echo $item->title;
+    }
+}
---
+++ js/list.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $.ajax({
+        type: "POST",
+        url: 'controllers/searchItems.php',
+        data: {
+            username: "Hey"
+        },
+        success: function(data)
+        {
+            alert(data);
+        },
+        error: function(data){
+            alert("error:"+data);
+        }
+    });
+    
+});
