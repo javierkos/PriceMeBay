b5766bfe7dc63f586fcac10b62b5f6c7e9838ac8
javierkos <zcabpas@ucl.ac.uk>
2018-01-15 07:11:49 +0000

    Added list elements
    Added functionality of searching elements in ebay and printing 10
    results

--- js/list.js
+++ js/list.js
@@ -7,7 +7,30 @@ $(document).ready(function() {
         },
         success: function(data)
         {
-            alert(data);
+            var parsedData = JSON.parse(data);
+            for (i = 0; i < 10; i++) { 
+                $("#products").append(`<div class="item  col-xs-4 col-lg-4">
+                <div class="thumbnail">
+                    <img class="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
+                    <div class="caption">
+                        <h4 class="group inner list-group-item-heading">`
+                            +data[i]+`</h4>
+                        <p class="group inner list-group-item-text">
+                            Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                            sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                        <div class="row">
+                            <div class="col-xs-12 col-md-6">
+                                <p class="cost">
+                                    $21.000</p>
+                            </div>
+                            <div class="col-xs-12 col-md-6">
+                                <a class="btn btn-success" href="http://www.jquery2dotnet.com">Compare</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`);
+            }
         },
         error: function(data){
             alert("error:"+data);
