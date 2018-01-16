515c0660afc451b44a90d0bd72bb87e23ac01cd0
javierkos <zcabpas@ucl.ac.uk>
2018-01-16 19:36:58 +0000

    Test

--- js/list.js
+++ js/list.js
@@ -15,6 +15,7 @@ $(document).ready(function() {
             },
             success: function(data)
             {
+                console.log(data);
                 //alert(data);
             var parsedData = JSON.parse(data);
             numElem = parsedData.length;
