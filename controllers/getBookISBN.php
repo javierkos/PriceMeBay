e99763b0e9a4c62cf676ba9949ed54bac19f18fb
javierkos <zcabpas@ucl.ac.uk>
2018-01-17 13:02:36 +0000

    Test

--- controllers/getBookISBN.php
+++ controllers/getBookISBN.php
@@ -11,9 +11,7 @@ use \DTS\eBaySDK\Trading\Enums;
 $service = new Services\TradingService([
     'credentials' => $config['production']['credentials'],
     'authToken'   => 'AgAAAA**AQAAAA**aAAAAA**gk5cWg**nY+sHZ2PrBmdj6wVnY+sEZ2PrA2dj6ADkoqmCpaHowydj6x9nY+seQ**jxkEAA**AAMAAA**Dz5I25WbPcRIjjjVrkiwlo+J2duxmVbOPTW3KeEmJ6irT7bWnXYGU0G5pOTy1Lj6qYsCuyyOUt9TO1Wj2qsimzDC8IbG6MvzzzNNkfKa2o2+Doosy5skfOQ1ZVKHrQV3wWu+4N3A1uWJMCtM5wwA216NlOnrZ3eoCiGp2j+kCUdjAe+nW9IGtFal6MCu2vjGpevrpn5irkH0KjRkwIxddMWFrEwTO/KcpVGL4aVVMI2tfgaaJuZyuLMC4J6eDYbtg2pDbSBDmPTVkEcnOd9foOO6ZYfhGzEOSrwfGZdKq2qH/e5etC7qAJpcu0XMoMacHcH0ClSjvxGjnoi1Mg4ANJnu4L7Zk4DFssDcQJ4d1AaNbuf7YKXzC1nkNdWor7Cm8ebJvCG9YvxffUwkNvKqvUOPmlx+jxL1IQEl35Y4sDEOGUm7aSImkK7iGY2VG4XHIXjqIdxju3Rs2oZGJQwLY3l7HllMV9JP0tl0t/CuZZHSsiCuxJjjGPI8frbC/ivmItA+c7FH5hVpKQsS6sU4YGT+QW2WuLy9SLmxvcKflM2WlXIL5tKe4E0LGbODrP2X+b/5Lz4ZHmL6qWXvUHTtAsTF1Gpk6bAqzBq8MnQAwRk8MwFXZhv2N6vqsgffNEVjNb31jyg1Iu2Cnb+H0278lo7oSUVnWWqwvnPoChGOg4ricRky2dSB1rL7VWb0uuM/MsgTJbHsBebca5i0mzQzammmXXSJVrTgNuxNOQQPlFOq7v6xBMdkqehbUXD7U6gT',
-    'siteId'    => Constants\SiteIds::GB,
-    'apiVersion' => '1043',
-    'sandbox' => false
+    'siteId'    => Constants\SiteIds::GB
 ]);
 
 function getISBN($refId){
